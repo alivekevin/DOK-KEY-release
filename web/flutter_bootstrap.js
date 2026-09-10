4452f12b0213bb9f54d7ac5f59b979ecf22d15ce
alivekevin <alivekevin@gmail.com>
2026-09-10 23:21:37 +0900

    feat(trivia): 3-strike survival mode (3 hearts), 10-question victory & bottom rules guide UI

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "221072944" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2654860965" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
