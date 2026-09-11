f55593982a4a4df04b9d08c66469a9534e3a2e4c
alivekevin <alivekevin@gmail.com>
2026-09-11 15:39:52 +0900

    fix: resolve Game 7/9 mobile canvas visibility, optimize controller clearance, and update landing copy (v4.8.4)

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4061623766" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4157995336" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
